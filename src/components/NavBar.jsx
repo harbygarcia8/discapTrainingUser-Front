273f93b23ce80ba4815f489d83c2dccb5719863d
harby garcia <harby-96@hotmail.com>
2023-04-03 23:00:30 -0500

    [Estilizando y creando SCSS, agregando NavBar, Estilizando página principal]

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
+import Logo from "../assets/img/Discap_Training_logotipo.png";
+import "../styles/NavBar.scss";
+import { information } from '../service/dataTest';
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>DISCAP TRAINING</h1>
-    </div>
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <img src={Logo} className="logo-dimension" />
+            </td>
+            <td className="text-right">
+              <div>
+                <ul className="text-right-ul">
+                  <li>
+                    <button className="SignIn">
+                    <a>Iniciar Sesión</a>
+                    </button>
+                    {/* <h6 className="h6-nav">
+                      <a>
+                        <i>Bienvenido </i>
+                        <i>Iniciar Sesión</i>
+                      </a>
+                    </h6> */}
+                  </li>
+                </ul>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {information ? (
+        <ul className="ul-nv">
+          <li className="ul-nv-li">
+            <a className="ul-nv-a" href="home">
+              Inicio
+            </a>
+          </li>
+          {information.data.hijos.map((navMenu, index) => (
+            <li key={index} className="ul-nv-li">
+              <a className="ul-nv-a">{navMenu.titulo}</a>
+              <ul className="ul-children">
+                {navMenu.hijos.map((innerMenu, index) => (
+                  <li key={index} className="ul-children-li">
+                    <a className="ul-children-a" href={`${innerMenu.titulo}`}>
+                      {innerMenu.titulo}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul className="ul-nv">
+          <li>
+            <a>Cargando...</a>
+          </li>
+        </ul>
+      )}
+    </>
   )
 }
 
---
+++ src/service/dataTest.js
@@ -0,0 +1,80 @@
+export const information = {
+    "status": {
+        "code": 200,
+        "description": "OK",
+        "message": null
+    },
+    "data": {
+        "codigo": 3075,
+        "nombre": "ROOT",
+        "titulo": "ROOT",
+        "accion": null,
+        "sistema": "MicrofrontEnd",
+        "padre": null,
+        "orden": null,
+        "hijos": [
+            {
+                "codigo": 3087,
+                "nombre": "Consultas",
+                "titulo": "Consultas",
+                "accion": null,
+                "sistema": "MicrofrontEnd",
+                "padre": 3075,
+                "orden": 1,
+                "hijos": [
+                    {
+                        "codigo": 3088,
+                        "nombre": "Consulta Afiliado",
+                        "titulo": "Consulta Afiliado",
+                        "accion": "Nueva",
+                        "sistema": "MicrofrontEnd",
+                        "padre": 3087,
+                        "orden": 1,
+                        "hijos": null
+                    },
+                    {
+                        "codigo": 3091,
+                        "nombre": "Consulta Expediente",
+                        "titulo": "Consulta Expediente",
+                        "accion": null,
+                        "sistema": "MicrofrontEnd",
+                        "padre": 3087,
+                        "orden": 2,
+                        "hijos": null
+                    }
+                ]
+            },
+            {
+                "codigo": 3089,
+                "nombre": "Procesos",
+                "titulo": "Procesos",
+                "accion": null,
+                "sistema": "MicrofrontEnd",
+                "padre": 3075,
+                "orden": 2,
+                "hijos": [
+                    {
+                        "codigo": 3090,
+                        "nombre": "Creación Expediente",
+                        "titulo": "Creación Expediente",
+                        "accion": null,
+                        "sistema": "MicrofrontEnd",
+                        "padre": 3089,
+                        "orden": 1,
+                        "hijos": null
+                    }
+                ]
+            },
+            {
+                "codigo": 3092,
+                "nombre": "Pruebas",
+                "titulo": "Pruebas",
+                "accion": null,
+                "sistema": "MicrofrontEnd",
+                "padre": 3075,
+                "orden": 3,
+                "hijos": []
+            }
+        ]
+    }
+}
