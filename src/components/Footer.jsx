6f7b30fe76387d77ca2800bfbd944ab042a4354c
harby garcia <harby-96@hotmail.com>
2023-05-16 21:45:01 -0500

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Footer.scss'
+import { Copyright } from '@mui/icons-material'
+
+function Footer() {
+  return (
+    <section className='container'>
+    <footer>
+        <h4>TODOS LOS DERECHOS SON RESERVADOS Y ÚNICAMENTE CON OBJETIVO DE APRENDIZAJE EN EL DESARROLLO WEB <Copyright></Copyright>2023</h4>
+    </footer>
+    </section>
+
+  )
+}
+
+export default Footer
