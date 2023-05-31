09efb9c7207cfa83fd64e9b4889ae158ae76bc49
Sebastian <25sebasr@gmail.com>
2023-05-31 15:12:17 -0500

    rutina de ejercicios

--- src/components/homePage/RouteTraining.jsx
+++ src/components/homePage/RouteTraining.jsx
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
-import {getVideobyUser} from '../../service/InfoVideoByUser'
+import { getVideobyUser } from '../../service/InfoVideoByUser'
 import '../../styles/InfoUser.scss'
+import '../../styles/TableUserDiscapacity.scss'
 
 const RouteTraining = (props) => {
   const [infoVideo, setInfoVideo] = useState();
@@ -25,41 +26,39 @@ const RouteTraining = (props) => {
   return (
     <>
       <section className="section-principal">
-      <section className="section-data">
-        <div className="section-header ">
-          <h3>VIDEOS DE ENTRENAMIENTOS</h3>
-        </div>
-        <div className="mainclientData-ul">
-        <Table responsive paging={true}>
-            <thead>
-              <tr className="title-modal">
-                <th>Titulo</th>
-                <th>Descripción</th>
-                <th>Discapacidad</th>
-                <th>Duración</th>
-                <th>Video</th>
-              </tr>
-            </thead>
-            <tbody>
-              { infoVideo && (
-                <tr className="content-modal">
-                  <td>{infoVideo[0].title}</td>
-                  <td>{infoVideo[0].descriptionVideo}</td>
-                  <td>{infoVideo[0].discapUser.discapacity}</td>
-                  <td>{infoVideo[0].duration}</td>
-                  <td>{infoVideo[0].url}</td>
-                </tr>
-              )
-                
-              }
-            </tbody>
-          </Table>
-        </div>
+        <section className="section-data">
+          <div className="section-header ">
+            <h3>VIDEOS DE ENTRENAMIENTOS</h3>
+          </div>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow className="table-header">
+                  <TableCell className="table-cell">Titulo</TableCell>
+                  <TableCell className="table-cell">Descripción</TableCell>
+                  <TableCell className="table-cell">Discapacidad</TableCell>
+                  <TableCell className="table-cell">Duración</TableCell>
+                  <TableCell className="table-cell">Video</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {infoVideo && infoVideo.map((video, index) => (
+                  <TableRow key={index} className="table-row">
+                    <TableCell className="table-cell">{video.title}</TableCell>
+                    <TableCell className="table-cell">{video.descriptionVideo}</TableCell>
+                    <TableCell className="table-cell">{video.discapUser.discapacity}</TableCell>
+                    <TableCell className="table-cell">{video.duration}</TableCell>
+                    <TableCell className="table-cell">
+                      <a href={video.url} target="_blank" className="link-black">Click para ver video</a>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </section>
       </section>
-    </section>
-    
-    
-  </>
+    </>
 
   )
 }
--- src/components/userDiscapacity/TableUserDiscapacity.jsx
+++ src/components/userDiscapacity/TableUserDiscapacity.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import '../../styles/TableUserDiscapacity.scss'
+import '../../styles/InfoUser.scss'
 
 
 function TableUserDiscapacity(props) {
@@ -9,43 +10,49 @@ function TableUserDiscapacity(props) {
   useEffect(() => {
     setData(props.data || []);
   }, [props.data]);
-  
+
   console.log(props.data)
   return (
     <>
-      <h2 className="table-title">Usuarios con Discapacidad</h2>
-      <TableContainer component={Paper} className="table-container">
-        <Table>
-          <TableHead>
-            <TableRow className="table-header">
-              <TableCell className="table-cell">Tipo de Documento</TableCell>
-              <TableCell className="table-cell">ID de Persona</TableCell>
-              <TableCell className="table-cell">Nombre</TableCell>
-              <TableCell className="table-cell">Apellido</TableCell>
-              <TableCell className="table-cell">Género</TableCell>
-              <TableCell className="table-cell">Teléfono</TableCell>
-              <TableCell className="table-cell">Email</TableCell>
-              <TableCell className="table-cell">Discapacidad</TableCell>
-              <TableCell className="table-cell">Estado</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((item, index) => (
-              <TableRow key={index} className="table-row">
-                <TableCell className="table-cell">{item.documentType}</TableCell>
-                <TableCell className="table-cell">{item.personID}</TableCell>
-                <TableCell className="table-cell">{item.name}</TableCell>
-                <TableCell className="table-cell">{item.surname}</TableCell>
-                <TableCell className="table-cell">{item.gender}</TableCell>
-                <TableCell className="table-cell">{item.phone}</TableCell>
-                <TableCell className="table-cell">{item.email}</TableCell>
-                <TableCell className="table-cell">{item.discapacity}</TableCell>
-                <TableCell className="table-cell">{item.active ? 'Activo' : 'Inactivo'}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <section className="section-principal">
+        <section className="section-data">
+          <div className="section-header ">
+            <h3>Usuarios con Discapacidad</h3>
+          </div>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow className="table-header">
+                  <TableCell className="table-cell">Tipo Documento</TableCell>
+                  <TableCell className="table-cell">ID de Persona</TableCell>
+                  <TableCell className="table-cell">Nombre</TableCell>
+                  <TableCell className="table-cell">Apellido</TableCell>
+                  <TableCell className="table-cell">Género</TableCell>
+                  <TableCell className="table-cell">Teléfono</TableCell>
+                  <TableCell className="table-cell">Email</TableCell>
+                  <TableCell className="table-cell">Discapacidad</TableCell>
+                  <TableCell className="table-cell">Estado</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((item, index) => (
+                  <TableRow key={index} className="table-row">
+                    <TableCell className="table-cell">{item.documentType}</TableCell>
+                    <TableCell className="table-cell">{item.personID}</TableCell>
+                    <TableCell className="table-cell">{item.name}</TableCell>
+                    <TableCell className="table-cell">{item.surname}</TableCell>
+                    <TableCell className="table-cell">{item.gender}</TableCell>
+                    <TableCell className="table-cell">{item.phone}</TableCell>
+                    <TableCell className="table-cell">{item.email}</TableCell>
+                    <TableCell className="table-cell">{item.discapacity}</TableCell>
+                    <TableCell className="table-cell">{item.active ? 'Activo' : 'Inactivo'}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </section>
+      </section>
     </>
   );
 }
