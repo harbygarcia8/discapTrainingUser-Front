8983a1f5906d5cdb382736d0669cfeb709fe80a1
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-06-03 00:48:06 -0500

    Modificacion IconButton

--- src/components/medicalHistory/MedicalHistoryTable.jsx
+++ src/components/medicalHistory/MedicalHistoryTable.jsx
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-import '../../styles/TableUserDiscapacity.scss'
-import '../../styles/InfoUser.scss'
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@mui/material";
+import "../../styles/TableUserDiscapacity.scss";
+import "../../styles/InfoUser.scss";
+import AddCircleTwoToneIcon from "@mui/icons-material/AddCircleTwoTone";
 
 function MedicalHistoryTable(props) {
-
   const [data, setData] = useState(props.data || []);
   useEffect(() => {
     setData(props.data || []);
@@ -12,49 +21,76 @@ function MedicalHistoryTable(props) {
   const items = props.data;
 
   return (
-    
     <section className="section-principal">
-        <section className="section-data">
-          <div className="section-header ">
-            <h3>Historias Clinicas</h3>
-          </div>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow className="table-header">
-                  <TableCell className="table-cell">Nombre</TableCell>
-                  <TableCell className="table-cell">Fecha de modificación</TableCell>
-                  <TableCell className="table-cell">Descipción médica</TableCell>
-                  <TableCell className="table-cell">Especialista asignado</TableCell>
-                  
+      <section className="section-data">
+        <div className="section-header ">
+          <h3>Historias Clinicas</h3>
+        </div>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow className="table-header">
+                <TableCell className="table-cell">Nombre</TableCell>
+                <TableCell className="table-cell">
+                  Fecha de modificación
+                </TableCell>
+                <TableCell className="table-cell">Descipción médica</TableCell>
+                <TableCell className="table-cell">
+                  Especialista asignado
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((item, index) => (
+                <TableRow key={index} className="table-row">
+                  <TableCell className="table-cell">
+                    {item.discapUser.name}
+                  </TableCell>
+                  <TableCell className="table-cell">
+                    {item.fechaGestion}
+                    {item.id}
+                  </TableCell>
+                  <TableCell className="table-cell">
+                    {item.descriptionMedicalHistory}
+                  </TableCell>
+                  <TableCell className="table-cell">
+                    {item.specialist.name}
+                  </TableCell>
+
+                  <TableCell className="table-cell">
+                    <a href="RutinaEntrenamiento">
+                      <Button
+                        variant="contained"
+                        size="medium"
+                        onClick={() => handleClickUser(item.id)}
+                        endIcon={<AddCircleTwoToneIcon />}
+                      >
+                        Añadir rutina entrenamiento
+                      </Button>
+                    </a>
+                    <br />
+                    <br />
+                    <a
+                      href="MedicalHistory"
+                      onClick={() => handleClickUser(item.id)}
+                    >
+                      <Button
+                        variant="contained"
+                        size="medium"
+                        endIcon={<AddCircleTwoToneIcon />}
+                      >
+                        Añadir Historia Clinica
+                      </Button>
+                    </a>
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.map((item, index) => (
-                  <TableRow key={index} className="table-row">
-                    <TableCell className="table-cell">{item.discapUser.name}</TableCell>
-                    <TableCell className="table-cell">{item.fechaGestion}{item.id}</TableCell>
-                    <TableCell className="table-cell">{item.descriptionMedicalHistory}</TableCell>
-                    <TableCell className="table-cell">{item.specialist.name}</TableCell>
-
-
-                    <TableCell className="table-cell"> 
-                      <a href='RutinaEntrenamiento'><button onClick={() => handleClickUser(item.id)}>Añadir rutina entrenamiento</button></a>
-                      <br/>
-                      <br/>
-                      <a href='MedicalHistory' onClick={() => handleClickUser(item.id)}><button>Añadir Historia Clinica</button></a>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </section>
-      </section> 
-
-
-
-  )
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </section>
+    </section>
+  );
 }
 
-export default MedicalHistoryTable
+export default MedicalHistoryTable;
